2798ca74fbb24b2ef072221b6a14f489cfcdf5e6
Joshua Kelly <jdjkelly@gmail.com>
2013-01-22 16:51:43 -0500

    Added contacts_info to raw omniauth request

--- lib/omniauth/strategies/yahoo.rb
+++ lib/omniauth/strategies/yahoo.rb
@@ -6,9 +6,9 @@ module OmniAuth
 
     # An omniauth 1.0 strategy for yahoo authentication
     class Yahoo < OmniAuth::Strategies::OAuth
-      
+
       option :name, 'yahoo'
-      
+
       option :client_options, {
         :access_token_path  => '/oauth/v2/get_token',
         :authorize_path     => '/oauth/v2/request_auth',
@@ -16,11 +16,11 @@ module OmniAuth
         :site               => 'https://api.login.yahoo.com'
       }
 
-      uid { 
+      uid {
         access_token.params['xoauth_yahoo_guid']
       }
-      
-      info do 
+
+      info do
         primary_email = nil
         if user_info['emails']
           email_info    = user_info['emails'].find{|e| e['primary']} || user_info['emails'].first
@@ -37,15 +37,16 @@ module OmniAuth
           }
         }
       end
-      
+
       extra do
         hash = {}
         hash[:raw_info] = raw_info unless skip_info?
+        hash[:contacts] = contacts_info
         hash
       end
 
-      # Return info gathered from the v1/user/:id/profile API call 
-     
+      # Return info gathered from the v1/user/:id/profile API call
+
       def raw_info
         # This is a public API and does not need signing or authentication
         request = "http://social.yahooapis.com/v1/user/#{uid}/profile?format=json"
@@ -54,10 +55,40 @@ module OmniAuth
         raise ::Timeout::Error
       end
 
+      # Provide the "Contacts" portion of the raw_info
+
+      def contacts_info
+        request = "http://social.yahooapis.com/v1/user/#{uid}/contacts?format=json"
+        @contacts_info ||= begin
+          _contacts_info = slim(MultiJson.decode(access_token.get(request).body))
+          @env['omniauth.contacts'] = _contacts_info
+          _contacts_info
+        end
+      rescue ::Errno::ETIMEDOUT
+        raise ::Timeout::Error
+      end
+
       # Provide the "Profile" portion of the raw_info
-      
+
       def user_info
         @user_info ||= raw_info.nil? ? {} : raw_info["profile"]
+      end
+
+      private
+
+      def slim(contacts)
+        _contacts = []
+
+        contacts['contacts']['contact'].each do |contact|
+          email, nickname = nil, nil
+
+          contact['fields'].each do |field|
+            email = field['value'] if field['type'] == 'email'
+            nickname = field['value'] if field['type'] == 'nickname'
+          end
+          _contacts << { :email => email, :nickname => nickname }
+        end
+        _contacts
       end
     end
   end
